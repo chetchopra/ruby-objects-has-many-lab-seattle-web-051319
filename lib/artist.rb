df105c4dc11581999f6020b277ec62388b31404d
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 21:53:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,32 @@
-# class Artist 
+class Artist 
   
-#   @@all = []
+  @@all = []
   
-#   attr_accessor :name, :songs
+  attr_accessor :name, :songs
   
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#     @@all << self
-#   end
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
   
-#   def add_song(song)
-#     @songs << song
-#     song.artist = self
-#   end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
   
-#   def add_song_by_name(song_name)
-#     song = Song.new(song_name)
-#     @songs << song
-#     song.artist = self
-#   end
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+  end
   
-#   def self.song_count
-#     total = 0
-#     @@all.each do |artist|
-#       total += artist.songs.length
-#     end
-#     return total
-#   end
+  def self.song_count
+    total = 0
+    @@all.each do |artist|
+      total += artist.songs.length
+    end
+    return total
+  end
   
-# end
+end
